e7b991ea4d38ffaae6145b3005a633d3293af982
John Kendrick <john@kendrick.me.uk>
2015-11-17 13:56:47 +0000

    Added information Windows to the README Modified the command.sh to use the command python rather than python2 Corrected the labelling of hdpe, mdpe and ldpe in pdielec

--- Examples/Castep/MgO/command.sh
+++ Examples/Castep/MgO/command.sh
@@ -1,1 +1,1 @@
-python2 ../../../pdielec  -vmin 300 -vmax 800 -sphere -needle 0 0 1 -dielectric 3  -vf 0.1 -vf 0.2 -method maxwell -method bruggeman  -method ap -sigma 10 -csv command.csv phonon
+python ../../../pdielec  -vmin 300 -vmax 800 -sphere -needle 0 0 1 -dielectric 3  -vf 0.1 -vf 0.2 -method maxwell -method bruggeman  -method ap -sigma 10 -csv command.csv phonon
