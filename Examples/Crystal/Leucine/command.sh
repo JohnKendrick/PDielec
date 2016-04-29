5f1ff420a86a59665d2e3f9b2bdc29689721f4f3
John Kendrick <john@kendrick.me.uk>
2016-04-29 19:30:18 +0100

    Adding CRYSTAL14 files

--- Examples/Crystal/Leucine/command.sh
+++ Examples/Crystal/Leucine/command.sh
@@ -1,1 +1,5 @@
-../../../pdielec -matrix ptfe -sigma 5 -plate 0 0 1 -ellipsoid 0 0 1 0.5 -plate 1 0 0 LEUCINE_FREQUENCY_PBED3_631Gdp_FULLOPTIMIZATON.out -optical 3 3 3  -csv command.csv
+../../../pdielec -matrix ptfe -sigma 5 \
+                 -plate 0 0 1 -ellipsoid 0 0 1 0.5 -plate 1 0 0 \
+                 -optical_tensor 2.3471 0 0.0786 0 2.3942 0 0.0786 0 2.2621 \
+                 -csv command.csv \
+                  LEUCINE_HP_DIELTENS_FREQUENCY_PBED_6311Gdp.out
