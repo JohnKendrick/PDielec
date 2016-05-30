f0631eb5a15eafcb51a73594bc1bbe891d626420
John Kendrick <john@kendrick.me.uk>
2016-05-30 14:44:39 +0100

    Modified to use the new -program interface

---
+++ Examples/AbInit/AlAs/command.sh
@@ -0,0 +1,4 @@
+python2 ../../../pdielec -matrix ptfe -sigma 5 \
+                 -plate 0 0 1 -ellipsoid 0 0 1 0.5 -plate 1 0 0 \
+                 -csv command.csv \
+                 -program abinit AlAs.out AlAs_anaddb.out
