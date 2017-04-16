38dfa368c6a00ab31244433c14854c5a4254a2a6
John Kendrick <john@kendrick.me.uk>
2017-04-16 16:10:16 +0100

    Modification to the command.sh file The previous command did not use the epsilon infinity from the calculation This does now. The reference output and csv files have been altered to reflect this

--- Examples/Crystal/ZnO/CPHF/command.sh
+++ Examples/Crystal/ZnO/CPHF/command.sh
@@ -1,4 +1,4 @@
 python ../../../../pdielec -vmin 300 -vmax 800 -matrix ptfe -sigma 5 \
                             -sphere -needle 0 0 1 -plate 1 0 0 \
-                            -optical 5.73897 5.73897 5.67346  -csv command.csv \
+                            -csv command.csv \
                             -hessian crystal -program crystal ZnO_CPHF.out   $*
