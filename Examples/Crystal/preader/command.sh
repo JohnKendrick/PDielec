195c0ff6250c4c1d907174a29e6c8624a5b7d784
John Kendrick <john@kendrick.me.uk>
2017-05-12 16:35:16 +0100

    New tests on the crystal style symmetrisation of the hessian in preader

--- Examples/Crystal/preader/command.sh
+++ Examples/Crystal/preader/command.sh
@@ -1,4 +1,4 @@
-python ../../../preader crystal  \
+python ../../../preader crystal -eckart  \
        ../Leucine/LEUCINE_DIELECTRC_PBED_6311Gdp.out \
        ../Leucine/LEUCINE_FREQUENCY_PBED3_631Gdp_FULLOPTIMIZATON.out \
        ../Na2SO42/Na2SO42.out \
