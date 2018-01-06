7ded1ca31fc716bae168380ec6a1fef343bff60d
John Kendrick <john@kendrick.me.uk>
2018-01-06 19:38:39 +0000

    New routines for writing the data to excel spreadsheet

--- Python/Plotter.py
+++ Python/Plotter.py
@@ -11,6 +11,7 @@ class Plotter:
         self.methods = []
         self.volume_fractions = []
         self.sizes = []
+        self.size_sigmas = []
         self.shapes = []
         self.shape_data = []
         self.plot_numbers = []
@@ -20,13 +21,14 @@ class Plotter:
         self.frequencies = []
         return
 
-    def add_dielectric(self, nplot, method, vf_type, size, shape, data, frequency, trace, absorption, molar_absorption):
+    def add_dielectric(self, nplot, method, vf_type, size, size_sigma, shape, data, frequency, trace, absorption, molar_absorption):
         """Add the trace of a dielectric to be plotted, add the absorption and the molar absorption"""
         if nplot not in self.plot_numbers:
             self.plot_numbers.append(nplot)
             self.methods.append(method)
             self.volume_fractions.append(vf_type)
             self.sizes.append(size)
+            self.size_sigmas.append(size_sigma)
             self.shapes.append(shape)
             self.shape_data.append(data)
             self.traces.append([])
@@ -39,6 +41,101 @@ class Plotter:
         self.molar_absorption_coefficients[nplot].append(molar_absorption)
         return
 
+    def excel(self, workbook):
+        """Print data to a xlsx file"""
+        import xlsxwriter as xlsx
+        #  Loop over the different sheets
+        worksheets = []
+        for itype,name in enumerate(['molar_absorption_coefficient','real_dielectric','imag_dielectric','absorption_coefficent']):
+          worksheet = workbook.add_worksheet(name)
+          worksheets.append(worksheet)
+          # Write a header
+          if itype == 0:
+            worksheet.write(0,0,'Molar Absorption Coefficients in cm-1.L.mol-1')
+          elif itype == 1:
+            worksheet.write(0,0,'Real component of the permittivity')
+          elif itype == 2:
+            worksheet.write(0,0,'Imaginary component of the permittivity')
+          elif itype == 3:
+            worksheet.write(0,0,'Absorption coefficient')
+          col = 1
+          # loop over the settings for each set of data
+          for method, shape, data, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.volume_fractions, self.sizes, self.size_sigmas):
+            col += 1; 
+            row = 0
+            row += 1; worksheet.write(row,0, 'Method'); worksheet.write(row,col, method)
+            row += 1; worksheet.write(row,0, 'Shape'); worksheet.write(row,col, shape)
+            row += 1; worksheet.write(row,0, 'Shape Data'); worksheet.write(row,col, data)
+            row += 1; worksheet.write(row,0, 'Mass or Volume fraction'); worksheet.write(row,col, vf_type)
+            row += 1; worksheet.write(row,0, 'Particle size um'); worksheet.write(row,col, size)
+            row += 1; worksheet.write(row,0, 'Sigma of size distribution'); worksheet.write(row,col, size_sigma)
+            strdata = str(data)
+            strdata = strdata.replace(" ", "")
+            strdata = strdata.replace(",", " ")
+            strdata = strdata.replace("'", "")
+            strdata = strdata.replace('"', "")
+            ssize=""
+            if size > 1.0e-6:
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
+            row += 1
+            worksheet.write(row,0, 'cm-1')
+            worksheet.write(row,1, 'THz')
+            if strdata == "":
+                worksheet.write(row,col,method+"/"+shape+"/"+name+"("+vf_type+ssize+")")
+            else:
+                worksheet.write(row,col,method+"/"+shape+"/"+strdata+"/"+name+"("+vf_type+ssize+")")
+            lastrow = row
+          # end loop over method.....
+        # end of loop over worksheets
+        # Use the first frequency list and write the frequencies in cm-1 and THz in each worksheet
+        for worksheet in worksheets:
+            col = 0
+            row = lastrow
+            for v in self.frequencies[0]:
+                thz = v * 0.0299792458
+                row += 1
+                worksheet.write(row,col,v)
+                worksheet.write(row,col+1,thz)
+        # Write entries in each worksheet
+        col = 1
+        for molar_absorption in self.molar_absorption_coefficients:
+            col += 1
+            row = lastrow
+            for entry in molar_absorption:
+                row += 1
+                worksheets[0].write(row,col,entry)
+            # end loop molar absorption
+        col = 1
+        for traces in np.real(self.traces):
+            col += 1
+            row = lastrow
+            for entry in traces:
+                row += 1
+                worksheets[1].write(row,col,entry)
+            # end loop dri
+        col = 1
+        for traces in np.imag(self.traces):
+            col += 1
+            row = lastrow
+            for entry in traces:
+                row += 1
+                worksheets[2].write(row,col,entry)
+            # end loop dri
+        col = 1
+        for absorption in self.absorption_coefficients:
+            col += 1
+            row = lastrow
+            for entry in absorption:
+                row += 1
+                worksheets[3].write(row,col,entry)
+            # end loop absorption
+        # end loop over frequency
+        return
     def printout(self, fd_csvfile, csv_extended):
         """Print data to a csv file"""
         if fd_csvfile == 0:
@@ -48,7 +145,7 @@ class Plotter:
         if csv_extended:
           output = "cm-1,thz"
         for outputType in ['_molar_absorption_coefficient_cm-1.L.mole-1','_real_dielectric','_imag_dielectric','_absorption_coefficent_cm-1']:
-          for method, shape, data, vf_type, size in zip(self.methods, self.shapes, self.shape_data, self.volume_fractions, self.sizes):
+          for method, shape, data, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.volume_fractions, self.sizes, self.size_sigmas):
             strdata = str(data)
             strdata = strdata.replace(" ", "")
             strdata = strdata.replace(",", " ")
@@ -56,7 +153,12 @@ class Plotter:
             strdata = strdata.replace('"', "")
             ssize=""
             if size > 1.0e-6:
-                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")+"mu"
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
             output += ","+method+"_"+shape+strdata+outputType+"("+vf_type+ssize+")"
           # end loop over method.....
         # end of loop over outputType
@@ -131,10 +233,15 @@ class Plotter:
         self.molar_absorption_coefficients[-1] = to_scale * molar_absorption
         plot_list = []
         plot_labels = []
-        for method, shape, data, frequencies, molar_absorption, vf_type, size in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.molar_absorption_coefficients, self.volume_fractions, self.sizes):
+        for method, shape, data, frequencies, molar_absorption, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.molar_absorption_coefficients, self.volume_fractions, self.sizes, self.size_sigmas):
             ssize=""
             if size > 1.0e-6:
-                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")+"mu"
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
             label = method+"_"+shape+str(data)+"("+vf_type+ssize+")"
             label = label.replace(" ", "")
             label = label.replace(",", " ")
@@ -167,10 +274,15 @@ class Plotter:
         self.absorption_coefficients[-1] = to_scale * absorptions
         plot_list = []
         plot_labels = []
-        for method, shape, data, frequencies, absorptions, vf_type, size in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.absorption_coefficients, self.volume_fractions, self.sizes):
+        for method, shape, data, frequencies, absorptions, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.absorption_coefficients, self.volume_fractions, self.sizes, self.size_sigmas):
             ssize=""
             if size > 1.0e-6:
-                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")+"mu"
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
             label = method+"_"+shape+str(data)+"("+vf_type+ssize+")"
             label = label.replace(" ", "")
             label = label.replace(",", " ")
@@ -204,10 +316,15 @@ class Plotter:
         self.traces[-1] = to_scale * traces
         plot_list = []
         plot_labels = []
-        for method, shape, data, frequencies, traces, vf_type, size in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.traces, self.volume_fractions, self.sizes):
+        for method, shape, data, frequencies, traces, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.traces, self.volume_fractions, self.sizes, self.size_sigmas):
             ssize=""
             if size > 1.0e-6:
-                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")+"mu"
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
             label = method+"_"+shape+str(data)+"("+vf_type+ssize+")"
             label = label.replace(" ", "")
             label = label.replace(",", " ")
@@ -241,10 +358,15 @@ class Plotter:
         self.traces[-1] = to_scale * traces
         plot_list = []
         plot_labels = []
-        for method, shape, data, frequencies, traces, vf_type, size in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.traces, self.volume_fractions, self.sizes):
+        for method, shape, data, frequencies, traces, vf_type, size, size_sigma in zip(self.methods, self.shapes, self.shape_data, self.frequencies, self.traces, self.volume_fractions, self.sizes, self.size_sigmas):
             ssize=""
             if size > 1.0e-6:
-                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")+"mu"
+                ssize = "/{:f}".format(size).rstrip("0").rstrip(".")
+                ssize += "mu"
+                if size_sigma:
+                    ssize =  "/{:f}".format(size).rstrip("0").rstrip(".")
+                    ssize += "/{:f}".format(size_sigma).rstrip("0").rstrip(".")
+                    ssize += "mu"
             label = method+"_"+shape+str(data)+"("+vf_type+ssize+")"
             label = label.replace(" ", "")
             label = label.replace(",", " ")
