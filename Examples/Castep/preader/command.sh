20dc11adcb1c13eb8b4f462edfeddaaf1d98d3ce
John Kendrick <john@kendrick.me.uk>
2018-01-09 16:23:36 +0000

    Added some Castep 17 files to the checking/testing system

--- Examples/Castep/preader/command.sh
+++ Examples/Castep/preader/command.sh
@@ -1,4 +1,4 @@
 python=$1
 shift
 params=$*
-$python ../../../preader $params -program castep -masses isotopic ../AsparticAcid/phonon.castep  ../MgO/phonon.castep  ../Na2SO42/phonon.castep
+$python ../../../preader $params -program castep -masses isotopic ../AsparticAcid/phonon.castep  ../MgO/phonon.castep  ../Na2SO42/phonon.castep 
