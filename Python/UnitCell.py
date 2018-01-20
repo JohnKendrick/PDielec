fde427eecfc9517e1bf2e7c056897b233c7b570a
John Kendrick <john@kendrick.me.uk>
2018-01-20 12:29:16 +0000

    A large routine has been added to find molecules in a unit cell. The new unit cell is returned along with the reordering index

--- Python/UnitCell.py
+++ Python/UnitCell.py
@@ -18,6 +18,8 @@
 """Hold unit cell information and its associated calculated properties"""
 
 import numpy as np
+import math
+from Python.Calculator import calculate_distance
 
 
 class UnitCell:
@@ -25,6 +27,12 @@ class UnitCell:
     def __init__(self, a=None, b=None, c=None, alpha=None, beta=None, gamma=None):
         self.fractional_coordinates = []
         self.xyz_coordinates = []
+        self.element_names = []
+        self.atom_labels = []
+        self.nions = 0
+        self.molecules = []
+        self.atomic_masses = []
+        self.centres_of_mass = []
         self.lattice = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
         if gamma is not None:
             self.lattice = self.convert_abc_to_unitcell(a, b, c, alpha, beta, gamma)
@@ -33,6 +41,38 @@ class UnitCell:
             self.lattice[1] = b
             self.lattice[2] = c
         self._calculate_reciprocal_lattice(self.lattice)
+
+    def print(self):
+        print('a,b,c,alpha,beta,gamma',self.a,self.b,self.c,self.alpha,self.beta,self.gamma)
+        print('lattice')
+        print(self.lattice)
+        print('reciprocal lattice')
+        print(self.reciprocal_lattice)
+        print('Fractional coords')
+        print(self.fractional_coordinates)
+        print('XYZ coords')
+        print(self.xyz_coordinates)
+        if self.molecules:
+            for molid,atoms in enumerate(self.molecules):
+                mass, cm_xyz, cm_frac = self.calculateCentreOfMass(atoms)
+                print('molecule ',molid+1)
+                print(atoms)
+                print('Mass',mass)
+                print('Centre of Mass (xyz)',cm_xyz)
+                print('Centre of Mass (frac)',cm_frac)
+
+    def calculateCentreOfMass(self,atoms):
+        mass = 0.0
+        cm = np.zeros(3)
+        for atom_index in atoms:
+            mass += self.atomic_masses[atom_index]
+            cm = cm + self.atomic_masses[atom_index] * self.xyz_coordinates[atom_index]
+        cm_xyz = cm / mass
+        cm_fractional = self.convert_xyz_to_abc(cm_xyz)
+        return mass, cm_xyz, cm_fractional
+
+    def set_atomic_masses(self, masses):
+        self.atomic_masses = masses
 
     def convert_unitcell_to_abc(self):
         """Convert a unit cell to the equivalent a, b, c, alpha, beta, gamma designation"""
@@ -52,7 +92,7 @@ class UnitCell:
         gamma = np.radians(gamma_degs)
         lattice = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
         lattice[2] = [0.0, 0.0, c]
-        lattice[1] = [0.0, b*np.sin(np.radians(alpha)), b*np.cos(alpha)]
+        lattice[1] = [0.0, b*np.sin(alpha), b*np.cos(alpha)]
         z = a * np.cos(beta)
         y = a * (np.cos(gamma) - np.cos(alpha)*np.cos(beta))/np.sin(alpha)
         x = a * np.sqrt(1.0 - np.cos(beta)**2 - (y/a)**2)
@@ -120,17 +160,237 @@ class UnitCell:
     def set_fractional_coordinates(self, coords):
         """Set the fractional coordinates and calculate the xyz coordinates"""
         self.fractional_coordinates = coords
-        self.xyz_coordinates = []
-        for abc in coords:
-            xyz = self.convert_abc_to_xyz(abc)
-            self.xyz_coordinates.append(xyz)
+        self.xyz_coordinates = self.convert_abc_to_xyz(coords)
+        self.nions = len(coords)
         return
 
     def set_xyz_coordinates(self, coords):
         """Set the xyz coordinates and calculate the fractional coordinates"""
         self.xyz_coordinates = coords
-        self.fractional_coordinates = []
-        for xyz in coords:
-            abc = self.convert_xyz_to_abc(xyz)
-            self.fractional_coordinates.append(abc)
+        self.fractional_coordinates = self.convert_xyz_to_abc(coords)
+        self.nions = len(coords)
         return
+
+    def set_element_names(self, element_names):
+        self.element_names = []
+        for el in element_names:
+          self.element_names.append(el)
+        return
+
+    def set_atom_labels(self, atom_labels):
+        self.atom_labels = []
+        for el in atom_labels:
+          self.atom_labels.append(el)
+        return
+
+    def find_symmetry(self):
+        """Find the space group symmetry of the unit cell"""
+        from spglib import get_spacegroup
+        cell = ( self.lattice, self.fractional_coordinates, self.atomic_nos )
+        self.spacegroup = get_spacegroup(cell, symmprec=1e-5)
+        print("Symmetry space group is", self.spacegroup)
+
+    def calculate_molecular_contents(self, scale, toler, covalent_radii):
+        """Find whole molecules in the unit cell
+           scale and toler are used to manipulate the covalent radii, thus the largest distance 
+           apart atoms i and j can be for a bond is
+           scale*( radi + radj) + toler """
+        # Calculate the contents of all the cells adjacent to the central cell
+        adjacents = ( 0, -1, 1 )
+        translations = [ (i, j, k) for i in adjacents for j in adjacents for k in adjacents ]
+        fractional_supercell = []
+        index_supercell = []
+        for tr in translations:
+            for l,a in enumerate(self.fractional_coordinates):
+                i,j,k = tr
+                index_supercell.append( l )
+                new_position = [ (xyz1 + xyz2)  for xyz1, xyz2 in zip(a, tr) ]
+                fractional_supercell.append( new_position )
+                #jk print('New positions ',new_position,l,i,j,k)
+        # Convert fractional supercell coordinates to xyz
+        # xyz_supercell will be an np array
+        xyz_supercell = np.empty_like(fractional_supercell)
+        for i,abc in enumerate(fractional_supercell):
+            xyz_supercell[i] = self.convert_abc_to_xyz(abc)
+        # put the atoms into boxes of boxSize
+        BoxAtoms = {}
+        BoxNeighbours = {}
+        boxSize = 0.0
+        scale = 1.1
+        toler = 0.1
+        # calculate boxsize
+        rmax = 0.0
+        for el in self.element_names:
+            #jk print("Element name",el)
+            if covalent_radii[el] > rmax:
+                rmax = covalent_radii[el]
+        boxSize = 2.0*scale*rmax + 0.5 + toler
+        #jk print('rmax = ',rmax)
+        # Put atoms into boxes and store the box info in Atom_box_id
+        Atom_box_id = []
+        for i,xyz in enumerate(xyz_supercell):
+            a = int( math.floor(xyz[0]/boxSize) )
+            b = int( math.floor(xyz[1]/boxSize) )
+            c = int( math.floor(xyz[2]/boxSize) )
+            abc = (a,b,c)
+            Atom_box_id.append(abc)
+            try:
+                BoxAtoms[abc].append(i)
+            except:
+                BoxAtoms[abc] = [i]
+        # Calculate the neighbouring boxes for each occupied box
+        for abc in BoxAtoms:
+            #jk print('Box ',abc, BoxAtoms[abc])
+            a,b,c = abc
+            BoxNeighbours[abc] = []
+            for i in [ -1, 0, 1]:
+              for j in [ -1, 0, 1]:
+                for k in [ -1, 0, 1]:
+                  BoxNeighbours[abc].append( (a+i,b+j,c+k) )
+        # end for abc in Box1
+         
+        # Calculate the bonding the supercell 
+        bondedToAtom = {}
+        for i,xyzi in enumerate(xyz_supercell):
+            #jk print('Calculating bonding to ',i,xyzi)
+            bondedToAtom[i] = []
+            # Find the element name for this atom in the supercell
+            ip = index_supercell[i]
+            i_el = self.element_names[ip]
+            # lookup all of the boxes that might hold a bonding atom
+            cell = Atom_box_id[i]
+            for abc in BoxNeighbours[cell]:
+                # lookup all the atoms that are in that cell
+                try: 
+                    for j in BoxAtoms[abc]:
+                        if j < i:
+                            # Find the element name for this atom in the supercell
+                            jp = index_supercell[j]
+                            j_el = self.element_names[jp]
+                            dist1 = scale*( covalent_radii[i_el] + covalent_radii[j_el]) + toler
+                            dist2 = calculate_distance(xyzi,xyz_supercell[j])
+                            if dist2 < dist1:
+                                bondedToAtom[i].append(j)
+                                bondedToAtom[j].append(i)
+                                #jk print('new bond', i, j, i_el, j_el, xyzi, xyz_supercell[j], dist1, dist2)
+                            # end if dist2 < dist1
+                        # end if j < i
+                    # end for j
+                except KeyError: 
+                    pass
+            # end for abc 
+        # end for i,a
+        #jk for atom_index in range(self.nions):
+            #jk print('Bonds from atom', atom_index, bondedToAtom[atom_index])
+        # 
+        # Now we have to find how many molecules we have in the cell
+        # There must be at least one molecule in the cell and it must contain the first atom
+        #
+        # BelongsToMolecule[i] holds which molecule the ith atom belongs to
+        belongsToMolecule = {}
+        # molecules is a dictionary of molecules each entry is a is a list of atoms
+        molecules = {}
+        molID = -1
+        # We stop when all the atoms in the original cell belong to a molecule
+        remainingAtoms = [ atom for atom in range(self.nions) ]
+        while len(remainingAtoms) > 0:
+            #jk print("Remaining atoms")
+            #jk print(remainingAtoms)
+            # create a new molecule from the first atom which has no molecule assigned to it
+            molID += 1
+            useAtom = remainingAtoms[0]
+            belongsToMolecule[useAtom] = molID
+            molecules[molID] = [ useAtom ]
+            remainingAtoms.remove(useAtom)
+            # Now using this new molecule with its only atom as a seed find any atoms connected to it
+            # We need to continue searching for bonded atoms until there are no more to be found
+            moreAtomsToBeFound = True
+            while moreAtomsToBeFound:
+                moreAtomsToBeFound = False
+                for i in range(len(xyz_supercell)):
+                    # has this atom been assigned a molecule yet?
+                    if i in belongsToMolecule:
+                        # atom i is already assigned to a molecule
+                        useThisMolecule = belongsToMolecule[i]
+                        #jk print("Using this molecule", useThisMolecule)
+                        # Go through all the atoms bonded to i and add to the current molecule
+                        for j in bondedToAtom[i]:
+                            jx = index_supercell[j]
+                            #jk print("atom j / jx is bonded to atom i",j,jx,i)
+                            # The image of j in the original cell might not be available, and j might be bonded
+                            if jx in remainingAtoms and not j in belongsToMolecule:
+                                # if j was not already in a molecule then we have new information
+                                moreAtomsToBeFound = True
+                                molecules[useThisMolecule].append(j)
+                                belongsToMolecule[j] = useThisMolecule
+                                #jk print("Removing atom index(j) from remaining atoms",index_supercell[j])
+                                remainingAtoms.remove(jx)
+                        # end for j
+                    # end if i in
+                # end for i
+            # while moreAtomsToBeFound
+        # until all the atoms belong to a molecule
+        #jk print('Number of molecules', molID+1)
+        self.centres_of_mass = []
+        for mol_index in molecules:
+            #jk print('Molecule ',mol_index)
+            #jk print('Atoms ',molecules[mol_index])
+            for atom_index in molecules[mol_index]:
+                index = index_supercell[atom_index]
+                #jk print('New atom index, old index', atom_index, index, self.element_names[index])
+            # Calculate centre of mass
+            mass = 0.0
+            cm = np.zeros(3)
+            for atom_index in molecules[mol_index]:
+                index = index_supercell[atom_index]
+                mass += self.atomic_masses[index]
+                cm = cm + self.atomic_masses[index] * xyz_supercell[atom_index]
+            cm_xyz = cm / mass
+            cm_fractional = self.convert_xyz_to_abc(cm_xyz)
+            self.centres_of_mass.append( cm_fractional )
+            #jk print('Mass', mass)
+            #jk print('Centre of mass', cm_fractional)
+        # Create a new unit cell with the atoms shifted so that whole molecules are ordered and within the cell
+        new_molecules = []
+        new_fractional = np.empty_like(self.fractional_coordinates)
+        new_element_names = []
+        new_index = 0
+        new_masses = []
+        old_order = []
+        for mol_index in molecules:
+            new_atom_index = []
+            cm = self.centres_of_mass[mol_index]
+            shift = np.array( [ 0.0, 0.0, 0.0] )
+            if cm[0] < 0.0:
+                shift += [ 1.0, 0.0, 0.0 ]
+            elif cm[0] > 1.0:
+                shift += [-1.0, 0.0, 0.0 ]
+            if cm[1] < 0.0:
+                shift += [ 0.0, 1.0, 0.0 ]
+            elif cm[1] > 1.0:
+                shift += [ 0.0,-1.0, 0.0 ]
+            if cm[2] < 0.0:
+                shift += [ 0.0, 0.0, 1.0 ]
+            elif cm[2] > 1.0:
+                shift += [ 0.0, 0.0,-1.0 ]
+            for atom_index in molecules[mol_index]:
+                old_index = index_supercell[atom_index]
+                old_order.append(old_index)
+                new_fractional[new_index] = shift + np.array(fractional_supercell[atom_index])
+                new_element_names.append(self.element_names[old_index])
+                new_masses.append(self.atomic_masses[old_index])
+                new_atom_index.append(new_index)
+                new_index += 1
+            new_molecules.append(new_atom_index)
+        new_unit_cell = UnitCell( self.a, self.b, self.c, self.alpha, self.beta, self.gamma )
+        new_unit_cell.set_fractional_coordinates(new_fractional.tolist())
+        new_unit_cell.set_element_names(new_element_names)
+        new_unit_cell.set_atomic_masses(new_masses)
+        new_unit_cell.set_molecules(new_molecules)
+        return new_unit_cell, old_order
+
+    def set_molecules(self, molecules):
+        '''Define a list of molecules, each molecule is a list of atom coordinates'''
+        self.molecules = molecules
+        return
+            
