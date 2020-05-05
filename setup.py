6447c5fc1f978d5892388bbfa5312dd71be71d43
John Kendrick <john@kendrick.me.uk>
2020-05-05 18:29:42 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PDielec",
-    version="6.3.1.dev2",
+    version="6.3.1",
     author="John Kendrick and Andrew Burnett",
     author_email="john@kendrick.co.uk",
     description="PDielec package for the calculation of THz and infrared spectra",
