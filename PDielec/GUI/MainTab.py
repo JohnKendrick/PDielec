8e46234d649782c5e511a5af486d7b7a2099684b
John Kendrick <john@kendrick.me.uk>
2021-11-02 17:10:09 +0000

    When a new file is read in the masses are reset to the average abundance

--- PDielec/GUI/MainTab.py
+++ PDielec/GUI/MainTab.py
@@ -225,6 +225,7 @@ class MainTab(QWidget):
         self.read_output_file()
         if self.notebook.settingsTab is not None:
             self.notebook.settingsTab.settings['Optical permittivity'] = None
+            self.notebook.settingsTab.settings['Mass definition'] = 'average'
             self.notebook.settingsTab.refresh(force=True)
         self.calculationRequired = False
         debugger.print('Finished:: on_calculation_button_clicked')
