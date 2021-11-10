6b9399bf00ffc3d70e8630e44d88f98a8b65ba5e
John Kendrick <john@kendrick.me.uk>
2021-11-10 12:20:53 +0000

    Added a new example of fitting ATR spectra

---
+++ Examples/ATR/Na2SO42_fit/script.py
@@ -0,0 +1,96 @@
+#
+# Handle the special case of the first scenario
+#
+self.notebook.switchScenario(0,scenarioType="Powder")
+#
+#
+#
+tab = self.notebook.mainTab
+tab.settings['Program'] = 'vasp'
+tab.settings['Output file name'] = 'OUTCAR'
+tab.settings['Excel file name'] = ''
+tab.settings['Script file name'] = 'atr_fitted_settings.py'
+tab.settings['QM program'] = 'vasp'
+#
+#
+tab = self.notebook.settingsTab
+tab.settings['Eckart flag'] =  True
+tab.settings['Neutral Born charges'] =  False
+tab.settings['Sigma value'] =  5
+tab.settings['Mass definition'] = 'average'
+tab.settings['Optical permittivity edited'] =  False
+tab.sigmas_cm1 = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10.0, 200.0, 5, 10.0, 5, 10.0, 5, 100.0, 5, 5, 35.0, 5, 130.0, 5, 100.0, 5]
+#
+#
+tab = self.notebook.scenarios[0]
+tab.settings['Legend'] = 'Scenario 1'
+tab.settings['Scenario type'] = 'Powder'
+tab.settings['Matrix'] = 'air'
+tab.settings['Matrix density'] =  0.001225
+tab.settings['Matrix permittivity'] =  1.0
+tab.settings['Bubble radius'] =  30.0
+tab.settings['Bubble volume fraction'] =  0.0
+tab.settings['Mass fraction'] =  0.999882372589573
+tab.settings['Volume fraction'] =  0.7999999999999295
+tab.settings['Particle size(mu)'] =  0.0001
+tab.settings['Particle size distribution sigma(mu)'] =  0.0
+tab.settings['Ellipsoid a/b'] =  1.0
+tab.settings['Unique direction - h'] =  0
+tab.settings['Unique direction - k'] =  0
+tab.settings['Unique direction - l'] =  1
+tab.settings['Mass or volume fraction'] = 'volume'
+tab.settings['ATR material refractive index'] =  4.0
+tab.settings['ATR theta'] =  45.0
+tab.settings['ATR S polarisation fraction'] =  0.5
+tab.settings['Effective medium method'] = 'Maxwell-Garnett'
+tab.settings['Particle shape'] = 'Sphere'
+#
+#
+tab = self.notebook.analysisTab
+tab.settings['Minimum frequency'] =  -1
+tab.settings['Maximum frequency'] =  400
+tab.settings['title'] = 'Analysis'
+tab.settings['Covalent radius scaling'] =  1.1
+tab.settings['Bonding tolerance'] =  0.1
+tab.settings['Bar width'] =  0.5
+#
+#
+tab = self.notebook.viewerTab
+tab.settings['Atom scaling'] =  0.5
+tab.settings['Maximum displacement'] =  1.0
+tab.settings['Bond colour'] =  [80, 80, 80, 255]
+tab.settings['Bond radius'] =  0.1
+tab.settings['Cell colour'] =  [255, 0, 0, 255]
+tab.settings['Cell radius'] =  0.1
+tab.settings['Background colour'] =  [120, 120, 120, 255]
+tab.settings['Arrow colour'] =  [0, 255, 0, 255]
+tab.settings['Arrow radius'] =  0.07
+tab.settings['Number of phase steps'] =  41
+tab.settings['Super Cell'] =  [1, 1, 1]
+#
+#
+tab = self.notebook.fitterTab
+tab.settings['Excel file name'] = 'ATR_Spectra_for_fitting.xlsx'
+tab.settings['Plot title'] = 'Experimental and Calculated Spectral Comparison'
+tab.settings['Fitting type'] = 'Minimise x-correlation'
+tab.settings['Number of iterations'] =  20
+tab.settings['Frequency scaling factor'] =  1.0
+tab.settings['Optimise frequency scaling'] =  True
+tab.settings['Spectrum scaling'] =  False
+tab.settings['Spectrum scaling factor'] =  1.0
+tab.settings['Independent y-axes'] =  False
+tab.settings['Spectral difference threshold'] =  0.05
+tab.settings['HPFilter lambda'] =  7.0
+tab.settings['Baseline removal'] =  False
+tab.settings['Scenario index'] =  0
+#
+#
+tab = self.notebook.plottingTab
+tab.settings['Minimum frequency'] =  300
+tab.settings['Maximum frequency'] =  1500
+tab.settings['Frequency increment'] =  0.2
+tab.settings['Molar definition'] = 'Unit cells'
+tab.settings['Number of atoms'] =  1
+tab.settings['Plot type'] = 'Powder ATR'
+tab.settings['concentration'] =  10.933228754034278
+tab.settings['cell concentration'] =  10.933228754034278
