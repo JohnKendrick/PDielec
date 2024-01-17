9bcd0ad4a98e757c8397c32f61d2ff57b8fa4104
John Kendrick <john@kendrick.me.uk>
2024-01-17 10:16:59 +0000

    Removed some duplicate code regarding the eckart check box

--- PDielec/GUI/SettingsTab.py
+++ PDielec/GUI/SettingsTab.py
@@ -94,11 +94,6 @@ class SettingsTab(QWidget):
         else:
             self.eckart_cb.setCheckState(Qt.Unchecked)
         self.eckart_cb.stateChanged.connect(self.on_eckart_changed)
-        if self.settings['Eckart flag']:
-            self.eckart_cb.setCheckState(Qt.Checked)
-        else:
-            self.eckart_cb.setCheckState(Qt.Unchecked)
-        self.eckart_cb.stateChanged.connect(self.on_eckart_changed)
         form.addRow(QLabel('Apply Eckart conditions?', self), self.eckart_cb)
         #
         # Add the Born neutral condition
