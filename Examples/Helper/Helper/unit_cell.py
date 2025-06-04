b3273ee338341ce5f7c73e2ee874a55925271802
John Kendrick <john@kendrick.me.uk>
2025-06-04 10:20:18 +0100

    Changes to the UnitCell api The calculation of the cell with whole molecules is now made in place, no new cell is created.

--- Examples/Helper/Helper/unit_cell.py
+++ Examples/Helper/Helper/unit_cell.py
@@ -28,25 +28,25 @@ def main():
     asparticAcid = getMaterial(os.path.join(examples_directory,'Castep','AsparticAcid','phonon.castep'))
     uc = asparticAcid.getCell()
     # bonds etc, are only defined if a new cell is created of whole molecules
-    newcell,number_of_molecules,old_atom_order = uc.calculate_molecular_contents()
+    number_of_molecules = uc.calculate_molecular_contents()
     print('Number of whole molecules {}'.format(number_of_molecules))
     print('CIF file')
-    newcell.write_cif('test.cif')
-    density = newcell.getDensity('cm')
+    uc.write_cif('test.cif')
+    density = uc.getDensity('cm')
     #
-    bonds,bondlengths = newcell.get_bonds()
+    bonds,bondlengths = uc.get_bonds()
     print('BONDS')
     print('=====')
     for (i,j),bondlength in zip(bonds,bondlengths):
         print('Bond {}-{} = {} Angstrom'.format(i,j,bondlength))
     #
-    angles,bondangles = newcell.get_bond_angles()
+    angles,bondangles = uc.get_bond_angles()
     print('BOND ANGLES')
     print('===========')
     for (i,j,k),bondangle in zip(angles,bondangles):
         print('Bond angles {}-{}-{} = {} degrees'.format(i,j,k,bondangle))
     #
-    torsions,torsionangles = newcell.get_torsions()
+    torsions,torsionangles = uc.get_torsions()
     print('TORSION ANGLES')
     print('==============')
     for (i,j,k,l),torsionangle in zip(torsions,torsionangles):
