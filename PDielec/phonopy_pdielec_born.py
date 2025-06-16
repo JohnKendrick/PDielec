5b71d2de8a7c74e3eb23b45998fc4daef34ad4ea
John Kendrick <john@kendrick.me.uk>
2025-06-16 19:16:06 +0100

    Modified the command line options to include -h, -output and -yaml Also if no outputfile is give the results are shown on the command line and the output can be redirected

--- PDielec/phonopy_pdielec_born.py
+++ PDielec/phonopy_pdielec_born.py
@@ -26,8 +26,8 @@ phonopy-pdielec-born BORN-PDIELEC
 
 import os
 import sys
-
 import phonopy
+from contextlib import nullcontext
 from phonopy.file_IO import parse_BORN
 
 
@@ -35,11 +35,15 @@ def show_usage():
     """Show the usage message."""
     print("")
     print("USAGE:")
-    print("phonopy-pdielec-born [--symprec 1.0E-5] [-input BORN] [-nosym] outputfilename")
-    print("")
+    print("phonopy-pdielec-born [--symprec 1.0E-5] [-input BORN] [-nosym] [-output BORN_PDIELEC] [-yaml phonopy.yaml] [outputfilename]")
     print(" -symprec Sets the precision used in determing the symmetry (default is 1.0E-5)")
-    print(" -input   Sets the input BORN charges file (Defaults is BORN)")
+    print(" -input   Sets the input Born charges file (Defaults is BORN)")
+    print(" -output  Sets the output Born charges file")
+    print(" -yaml    phonopy.yaml Sets the output Born charges file (Defaults is phonopy.yaml)")
     print(" -nosym   Stops symmetry being used in parsing the BORN file")
+    print("")
+    print(" If no output file is given the output goes to stdout       ")
+
 
 def main():
     """Define the main routine for the phonopy-pdielec-born command.
@@ -52,13 +56,11 @@ def main():
 
     Options:
     -symprec 1.0E-5       Sets the precision used in determing the symmetry (default is 1.0E-5)
-    -input   BORN         Sets the input BORN charges file (Defaults is BORN)
+    -input   BORN         Sets the input Born charges file (Defaults is BORN)
+    -output  BORN_PDIELEC Sets the output Born charges file 
+    -yaml    phonopy.yaml Sets the output Born charges file (Defaults is phonopy.yaml)
     -nosym                Stops symmetry being used in parsing the BORN file
     """
-    if len(sys.argv) <= 1:
-        show_usage()
-        sys.exit()
-
     symprec = 1.e-5
     inputfilename = "BORN"
     outputfilename = None
@@ -66,6 +68,7 @@ def main():
     tokens = sys.argv[1:]
     ntokens = len(tokens)
     itoken = 0
+    yamlfilename = "phonopy.yaml"
     while itoken < ntokens:
         token = tokens[itoken]
         if "-symprec" in token:
@@ -76,23 +79,33 @@ def main():
             inputfilename = tokens[itoken]
         elif "-nosym" in token:
             symmetry = False
+        elif "-output" in token:
+            itoken += 1
+            outputfilename = tokens[itoken]
+        elif "-yaml" in token:
+            itoken += 1
+            yamlfilename = tokens[itoken]
+        elif "-h" in token:
+            itoken += 1
+            show_usage()
+            sys.exit()
         else:
             outputfilename = token
         itoken += 1
-    print("Phonopy BORN charges are being converted to PDielec format")
-    print(f"The input BORN charges are from: {inputfilename}")
-    print(f"The symmetry precision is      : {symprec}")
-    print(f"The final output is in         : {outputfilename}")
-    if outputfilename is None:
+    print("Phonopy BORN charges are being converted to PDielec format",file=sys.stderr)
+    print(f"The input BORN charges are from: {inputfilename}",file=sys.stderr)
+    print(f"The phonopy.yaml is            : {yamlfilename}",file=sys.stderr)
+    print(f"The symmetry precision is      : {symprec}",file=sys.stderr)
+    print(f"The final output is in         : {outputfilename}",file=sys.stderr)
+    if outputfilename and os.path.exists(outputfilename):
         print("")
-        print("No output file has been given")
-        show_usage()
+        print("Error output file already exists",file=sys.stderr)
         sys.exit()
-    if os.path.exists(outputfilename):
+    if not os.path.exists(yamlfilename):
         print("")
-        print("Error output file already exists")
+        print("Error phonopy.yaml does not exist",file=sys.stderr)
         sys.exit()
-    phonon = phonopy.load("phonopy.yaml")
+    phonon = phonopy.load(yamlfilename)
     primitive = phonon.primitive
     nac = parse_BORN(primitive, symprec=symprec, is_symmetry=symmetry, filename=inputfilename)
     borns = nac["born"]
@@ -100,7 +113,8 @@ def main():
     text = "# epsilon and Z* of atoms PDielec/PDGui file format "
     lines = [text]
     lines.append(("%13.8f " * 9) % tuple(epsilon.flatten()))
-    with open(outputfilename,"w") as fd:
+
+    with open(outputfilename, "w") if outputfilename else nullcontext(sys.stdout) as fd:
         for z in borns:
             lines.append(("%13.8f " * 9) % tuple(z.flatten()))
         print("\n".join(lines),file=fd)
